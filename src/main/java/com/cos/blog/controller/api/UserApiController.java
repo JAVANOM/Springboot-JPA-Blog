20862ab08a7e6ad7b07de1c4f938bcb257ffadc4
mg <lkcbap12@gmail.com>
2023-10-30 23:09:23 +0900

    44강 finish

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,6 @@
 package com.cos.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,18 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
 
 //데이터만 
 @RestController
 public class UserApiController {
     
+	@Autowired
+	private UserService userService;
+	
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		
 		System.out.println("APIController");
 		
 		// 실제로 DB에 insert를 하고 아래에서 return이 되게 한다
-		return new ResponseDto<Integer>(HttpStatus.OK, 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+
+		int result = userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), result); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 		
 		//return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
